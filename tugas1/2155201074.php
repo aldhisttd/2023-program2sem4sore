b000689e4c6ccd9429371350781129aa3d3a7df6
Raja Hariadi <rajahariadi0@gmail.com>
2023-04-29 04:27:18 +0700

    Tugas 1 Raja Hariadi

---
+++ tugas1/2155201074.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS 1</title>
+</head>
+<body>
+    <h2>Form Username dan Password </h2>
+    <form>
+    <table>
+        <tr>
+            <td><label for="username">Username</label></td>
+            <td><input id="username" type="text" /></td>
+        </tr>
+        <tr>
+            <td><label for="password">Password</label></td>
+            <td><input id="password" type="password" /></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" value="Submit" /></td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
