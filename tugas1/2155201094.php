18d232c2d2439cf87b65d352b3c0318f4be901e2
ayukumaladewi <ayuk50362@gmail.com>
2023-04-28 19:20:56 +0700

    tugas ayu

---
+++ tugas1/2155201094.php
@@ -0,0 +1,19 @@
+<h3>  FROM MAHASISWA </h3>
+
+<form action="" method="post">
+<table>
+    <tr>
+        <td with="130">Username</td>
+        <td><input type="text" name="Username"></td>
+    </tr>
+    <tr>
+        <td> Password </td>
+        <td> <input type="text" name="Password"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="submit" value="Masuk" name="proses"></td>
+        
+    </tr>
+
+</table>
