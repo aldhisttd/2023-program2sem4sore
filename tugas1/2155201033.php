f8552f09e36e42fbc4488483e6a4a6fd686d06e2
KrisdiantoHuang <krisdiantohuang@gmail.com>
2023-04-29 14:49:31 +0700

    Tugas Krisdianto

---
+++ tugas1/2155201033.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Form Login</h2>
+    <td>Username</td>
+    <td>:</td>
+    <td><input type = "text" placeholder="Full Name"></td>
+    <p>
+    <td>Password</td>
+    <td>:</td>
+    <td><input type = "password" placeholder="Enter Password"></td>
+    <p>
+    <td><input type="submit" value="Login"></td>
+</body>
+</html>
