fdae38998adc44d8d02c6bd367f85fea5e3f29b9
zaky10062001 <zakytjr5@gmail.com>
2023-04-28 21:43:30 -0700

    tugas Muhammad Zaky

---
+++ tugas1/2155201002.php.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Selamat Datang</title>
+    </head>
+    <body>
+        <div class="container">
+          <h1>FORM MAHASISWA</h1>
+                <input name="tujuan" type="hidden" value="LOGIN" >
+
+                <label>Username</label>
+                <br>
+                <input name="username" type="text">
+                <br>
+                <label>Password</label>
+                <br>
+                <input name="password" type="password">
+                <br>
+                <button>Submit</button>
+        </div>
+    </body>
+</html>
