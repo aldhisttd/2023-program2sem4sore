e79230174c31a62b9165081746267ec9ee3edfc4
Arini nurhidayati <131981558+Arini261203@users.noreply.github.com>
2023-05-03 19:44:45 +0700

    Tugas Arini
    Mohon koreksinya pak

---
+++ tugas1/2155201051.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS 1</title>
+</head>
+<body>
+    <h1>FORM</h1>
+    <form action="">
+        <label for="username">Username</label><br>
+        <input type="text" name="username" id="username"><br><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password"><br><br>
+        <button type="submit">Log in</button>
+    </form>
+</body>
+</html>
