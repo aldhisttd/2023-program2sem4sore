16b1a8fda9bf65c55cf94da0308db06475903fa4
unknown <ulfasartika005@gmail.com>
2023-05-03 11:36:41 +0700

    html form uname dan pass

---
+++ tugas1/2155201078.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie-edge">
+    <meta name="viewport" content="width=device-width", initial-scale="1.0">
+    <title>Tugas 1</title>
+</head>
+
+<body>
+    <div class="login-box">
+        <h2>Login</h2>
+        <form action="">
+            <div class="input-box">
+                <input type="text" required="">
+                <label>Username</label>
+            </div>
+            <div class="input-box">
+                <input type="password" required="">
+                <label>Password</label>
+            </div>
+            <div class="forgot-pass">
+                <a href="">Forgot Your Password?</a>
+            </div>
+            <button type="submit" class="btn">Login</button>
+
+        </form>
+    </div>
+</body>
+</html>
