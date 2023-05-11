e5e80bc3eacfc44b8cb965d3e9a72337266e1062
alippratama <70047830+alippratama@users.noreply.github.com>
2023-05-11 12:19:24 +0700

    Create tugas alip.php

---
+++ tugas alip.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Cara Membuat Form Login Sederhana Tapi Berkelas</title>
+</head><l
+<body>
+<div class="login">
+    <h2 class="login-header">Form Login</h2>
+        <form class="login-container">
+            <p>
+                <input type="email" placeholder="Email">
+            </p>
+            <p>
+                <input type="password" placeholder="Password">
+            </p>
+            <p>
+                <input type="submit" value="Log in">
+            </p>
+        </form>
+</div>
+</body>
+</html>
