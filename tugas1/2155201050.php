6b487716e43a5d69a34815fe009c66122b451cab
septiaprstka <septia.prastika0409@gmail.com>
2023-05-11 20:20:39 +0700

    edit2

--- tugas1/2155201050.php
+++ tugas1/2155201050.php
@@ -1,14 +1,9 @@
-<form action="login.php" method="POST">
-  <fieldset>
-    <legend>Login</legend>
-    <p>
-      <label>Username:</label>
-      <input type="text" name="username" placeholder="username..." />
-    </p>
-    <p>
-      <label>Password:</label>
-      <input type="password" name="password" placeholder="password..." />
-    </p>
-
-  </fieldset>
-</form>
+<legend>Login</legend>
+<p>
+  <label>Username:</label>
+  <input type="text" name="username" placeholder="username..." />
+</p>
+<p>
+  <label>Password:</label>
+  <input type="password" name="password" placeholder="password..." />
+</p>
