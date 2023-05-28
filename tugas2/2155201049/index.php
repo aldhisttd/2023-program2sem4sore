0ad60c890a217233753d1d0fe5e0c5fdcec3f1d1
Siti Nadhila <stndhla07@gmail.com>
2023-05-28 18:36:10 +0700

    Siti Nadhila

---
+++ tugas2/2155201049/index.php
@@ -0,0 +1,41 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Laptop Baru</title>
+   
+</head>
+
+<body>
+
+    <h1>Form Data Laptop</h1>
+
+    <form action="proses.php" method="POST">
+
+
+        <label for="">Merk</label><br>
+        <input type="text" name="merk">
+
+        <br><br>
+
+        <label for="">Spesifikasi</label> <br>
+        <textarea id="spesifikasi" name="spek" rows="4" cols="50"></textarea>
+
+        <br><br>
+
+        <label for="">Harga</label> <br>
+        <input type="number" id="" name="harga">
+
+        <br><br>
+
+        <label for="">Gambar</label> <br>
+        <input type="file" id="" name="gambar">
+
+        <br><br>
+
+        <input type="submit" name="input" value="input">
+    </form>
+</body>
+</html>
