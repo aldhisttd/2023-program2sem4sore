412211d90311d9ada6fd17aaa711ea2fb3e71060
imamzhdi03 <imamzuhdi28@gmail.com>
2023-06-07 00:45:51 -1000

    “tugas02”

--- tugas2/2155201072/index.php
+++ tugas2/2155201072/index.php
@@ -7,34 +7,23 @@
     <title>Document</title>
 </head>
 <body>
-
     <h1>Form Data Pasien</h1>
-
     <form action="proses.php" method="POST">
-
         <label for="">NIK</label><br>
         <input type="text" name="nik">
-
         <br>
-
         <label for="">Nama Pasien</label> <br>
         <input type="text" name="nama">
-
         <br>
-
         <label for="">Jenis</label> <br>
         <select name="jenis">
             <option value="">-</option>
             <option value="bpjs">BPJS</option>
             <option value="umum">Umum</option>
             <option value="vip">VIP</option>
-
         </select>
-
         <br><br>
-
         <input type="submit" name="btn_submit" value="Simpan">
-
     </form>
     
 </body>
