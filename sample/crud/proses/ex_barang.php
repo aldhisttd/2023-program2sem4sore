6389692ff7d056276d7ebdc1eb03e6d255aae5b1
Aldhi <aldhi.albadri@gmail.com>
2023-06-08 20:51:19 +0700

    add sample crud

---
+++ sample/crud/proses/ex_barang.php
@@ -0,0 +1,21 @@
+<?php 
+
+$kdBrg = $_POST['kdbrg'];
+$nama = $_POST['namabrg'];
+$stn = $_POST['satuan'];
+$hrg = $_POST['hrg'];
+
+
+$namagbr = basename($_FILES["gbr"]["name"]);
+$target_file = "../gambar/". basename($_FILES["gbr"]["name"]);
+
+$upload = move_uploaded_file($_FILES["gbr"]["tmp_name"], $target_file);
+
+include "koneksi.php";
+
+$q = "INSERT INTO barang (kode_barang, nama, satuan, harga, gambar) VALUES('$kdBrg','$nama','$stn',$hrg,'$namagbr')";
+
+mysqli_query($koneksi, $q);
+
+header("location:../data.php");
+
---
+++ sample/crud/proses/koneksi.php
@@ -0,0 +1,3 @@
+<?php
+
+$koneksi = mysqli_connect('127.0.0.1', 'root', 'root', '4sore', '3307');
