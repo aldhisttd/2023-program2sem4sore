0646b5fc926fdb0005ced8bd68dd143c52907d97
Aldhi <aldhi.albadri@gmail.com>
2023-06-15 20:54:34 +0700

    add Update dan DElete

--- sample/crud/data.php
+++ sample/crud/data.php
@@ -21,6 +21,7 @@ tr:nth-child(even) {
 </style>
 </head>
 <body>
+<?php include "component/menu.php" ?>
 
 <h2>Data Barang</h2>
 
@@ -33,7 +34,9 @@ tr:nth-child(even) {
     <th>Gambar</th>
     <th>Action</th>
   </tr>
+
   <?php 
+    
     include "proses/koneksi.php";
     $data = mysqli_query($koneksi, "SELECT * FROM barang");
 
@@ -48,8 +51,8 @@ tr:nth-child(even) {
                 <img src="gambar/<?php echo $row['gambar'] ?>" width="100">
             </td>
             <td>
-                <a href="#">Hapus</a> - 
-                <a href="#">Edit</a>
+                <a href="hapus.php?kode=<?php echo $row['kode_barang'] ?>">Hapus</a> - 
+                <a href="edit.php?kode=<?php echo $row['kode_barang'] ?>">Edit</a>
             </td>
         </tr>
 
--- sample/crud/index.php
+++ sample/crud/index.php
@@ -7,6 +7,8 @@
     <title>Document</title>
 </head>
 <body>
+
+    <?php include "component/menu.php" ?>
     
     <form action="proses/ex_barang.php" method="POST" enctype="multipart/form-data">
 
