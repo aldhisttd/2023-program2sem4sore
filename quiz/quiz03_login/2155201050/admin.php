3acb75a5ae73aa98e71045bc696479bf5d0e0a69
septiaprstka <septia.prastika0409@gmail.com>
2023-07-13 23:14:47 +0700

    quiz03/login_2155201050

---
+++ quiz/quiz03_login/2155201050/admin.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header('location:login.php');
+}
+
+if ($_SESSION['role'] == 'karyawan') {
+    header('location:karyawan.php');
+}
+
+if ($_SESSION['role'] == 'user') {
+    header('location:user.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+    <h1>Halaman Admin</h1>
+    <p>Login sebagai : <?php echo $_SESSION['login'] ?></p>
+    <p><a href="logout.php">Logout</a></p>
+</body>
+</html>
