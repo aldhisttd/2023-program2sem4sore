0f48308f1bc7e50a4b312598802271594bfd981e
ayukumaladewi <ayuk50362@gmail.com>
2023-07-13 23:49:37 +0700

    quiz02/genap_2155201094

---
+++ quiz/quiz02_crud/2155201094/data.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+<?php include "component/menu.php" ?>
+
+<h2>Data Barang</h2>
+
+<table>
+  <tr>
+    <td>ISBN</td>
+    <td>Judul</td>
+    <td>Kategori</td>
+    <td>Stok</th>
+    <td>File Ebook</th>
+  </tr>
+
+  <?php 
+    
+    include "proses/koneksi.php";
+    $data = mysqli_query($koneksi, "SELECT * FROM buku");
+
+    while($row = mysqli_fetch_array($data)){
+    ?>
+        <tr>
+          <td><?php echo $row['isbn'] ?></td>
+            <td><?php echo $row['judul'] ?></td>
+            <td><?php echo $row['kategori'] ?></td>
+            <td><?php echo $row['stok'] ?></td>
+            <td>
+                <a href="file/<?php echo $row['file_ebook']?>">Download</a>
+            </td>
+            <td>
+                <a href="hapus.php?kode=<?php echo $row['isbn'] ?>">Hapus</a> - 
+                <a href="edit.php?kode=<?php echo $row['isbn'] ?>">Edit</a>
+            </td>
+        </tr>
+
+    <?php
+    }
+    ?>
+</table>
+
+</body>
+</html>
+
