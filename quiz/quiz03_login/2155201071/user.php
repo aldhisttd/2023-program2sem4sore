e2fbcdfd5fafc8170149c7241da1c17b203fc6f0
ameliasyyafitri29 <ameliasyafittrilx8@gmail.com>
2023-07-20 16:08:01 +0700

    quiz03/login_2155201071

---
+++ quiz/quiz03_login/2155201071/user.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header('location:form_login.php');
+}
+if ($_SESSION['role'] == 'admin') {
+    header('location:admin.php');
+}
+if ($_SESSION['role'] == 'karyawan') {
+    header('location:karyawan.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+</head>
+<body>
+    <h1>Halaman User</h1>
+    <p>Login sebagai : <?php echo $_SESSION['login'] ?></p>
+    <p><a href="logout.php">Logout</a></p>
+
+</body>
+</html>
