4a168b37dea54c612a798f7b34646982f4f293f8
jodihermansyah <jodihsitumorang@gmail.com>
2023-07-29 17:54:26 +0700

    perbaikan/logindb/2155201090/jodi hermansyah situmorang

---
+++ quiz/perbaikan/logindb/2155201090/user.php
@@ -0,0 +1,24 @@
+<?php
+// user.php
+
+// Mulai sesi
+session_start();
+
+// Periksa apakah pengguna telah login dan memiliki peran sebagai user_ganjil
+if (isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'user_ganjil') {
+    echo "Selamat Datang di Halaman User";
+} else {
+    header("Location: login.php"); // Redirect ke halaman login jika pengguna belum login atau tidak memiliki peran user_ganjil
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman User</title>
+</head>
+<body>
+    <h1>User Ganjil</h1>
+    <P>  <a href="admin.php">Halaman admin & user</a></P>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
