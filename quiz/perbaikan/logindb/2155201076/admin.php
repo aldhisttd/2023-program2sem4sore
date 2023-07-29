b05eb4ef6520ec9896789bd7608aa577d9c838f7
sajidatulfaalawiyahh <sajidatulfaalawiyah7@gmail.com>
2023-07-30 00:24:26 +0700

    perbaikn/logindb/2155201076/Sajidatulfa Alawiyah

---
+++ quiz/perbaikan/logindb/2155201076/admin.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header('location:login.php');
+}
+if ($_SESSION['role'] == 'user_genap') {
+    header('location:user.php');
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+    <h1>Halaman Admin</h1>
+    <p>Login sebagai : <?php echo $_SESSION['role'] ?></p>
+    <p><a href="user.php">Halaman User</a></p>
+    <p><a href="logout.php">Logout</a></p>
+</body>
+</html>
