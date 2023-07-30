aa5d377cd925027cc6ea1ef4eccf339fccbf8a97
dewi anggraini <dewiangrini05@gmail.com>
2023-07-30 16:35:59 +0700

    perbaikan/crud/2155201004/oni_julaika

---
+++ quiz/perbaikan/crud/2155201004/data.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Software</title>
+
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #000;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+
+<body>
+    <h1> Data Laptop</h1>
+    <a href="index.php">Tambah Data</a><br><br>
+
+    <table>
+        <tr>
+            <th>SN</th>
+            <th>Tanggal Rilis</th>
+            <th>Jenis</th>
+            <th>Quantity</th>
+            <th>Spesifikasi</th>
+            <th>Gambar</th>
+            <th>action</th>
+        </tr>
+        <?php
+        include "koneksi.php";
+        $data = mysqli_query($koneksi, "SELECT * FROM software");
+        while ($row = mysqli_fetch_assoc($data)) {
+        ?>
+            <tr>
+                <td><?php echo $row['sn']; ?></td>
+                <td><?php echo $row['tanggal_rilis']; ?></td>
+                <td><?php echo $row['jenis']; ?></td>
+                <td><?php echo $row['quantity']; ?></td>
+                <td><?php echo $row['spesifikasi']; ?></td>
+                <td>
+                    <img src="gambar/<?php echo $row['gambar']; ?>" width="150">
+                </td>
+                <td>
+                    <a href="edit.php?kode=<?php echo $row['sn']; ?>">Edit</a> -
+                    <a href="hapus.php?kode=<?php echo $row['sn']; ?>" onclick="return confirm('Yakin ingin hapus?')">Hapus</a>
+                </td>
+            </tr>
+        <?php }; ?>
+
+    </table>
+</body>
+
+</html>
