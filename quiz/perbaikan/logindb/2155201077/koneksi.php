b7fc8c5384097d3839a91f17f3908df6f2a7a467
ReiCahyaRani <reycahayarani598@gmail.com>
2023-07-30 17:49:26 +0700

    perbaikan/logindb/2155201077/Rei Cahya Rani

--- quiz/perbaikan/logindb/2155201077/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_SERVER = "localhost";
-$user ="root";
-$pass ="";
-$database ="perbaikanquiz";
-$koneksi = mysqli_connect('localhost', 'root', '', 'perbaikanquiz');
-
-
-?>
--- quiz/perbaikan/logindb/2155201077/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-
-unset($_SESSION['admin_ganjil']);
-unset($_SESSION['user_ganjil']);
-
-
-header('location:login.php')
-?>
